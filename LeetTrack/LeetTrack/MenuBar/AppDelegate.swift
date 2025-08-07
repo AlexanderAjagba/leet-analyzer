592e8ba50ac127afa9c36fbd85059f181cbf5124
Alexander Ajagba <alexanderajagba21@gmail.com>
2025-08-06 21:17:10 -0400

    fixed app-popover
    fixed missing app-popover

--- LeetTrack/LeetTrack/MenuBar/AppDelegate.swift
+++ LeetTrack/LeetTrack/MenuBar/AppDelegate.swift
@@ -1,31 +1,32 @@
 import Foundation
 import SwiftUI
+import AppKit    // ← needed for NSStatusBar, NSImage, etc.
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusItem: NSStatusItem!
     var appPopover: AppPopover!
-
+    
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // 1. Set up the status bar item with a title
+        // 1. Create the status‐bar item
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-
+        
         if let button = statusItem.button {
-            if let image = NSImage(named: "leetcode_menubar") {
-                image.isTemplate = true
-                button.image = image
-            } else {
-                print("Failed to load image")
-            }
+            // 2. Load your image (replace "MyMenuBarIcon" with your asset name)
+            let icon = NSImage(resource: .image)
+            icon.isTemplate = true            // so it adapts to light/dark mode
+            button.image = icon               // ← assign it here
             button.imagePosition = .imageOnly
+            
+            // wire the button to call your toggle
+            button.action = #selector(togglePopover(_:))
+            button.target = self
         }
         
+        // initialize your popover
         appPopover = AppPopover(contentView: ContentView())
+    }   // ← Make sure this brace closes applicationDidFinishLaunching
 
-        // Button action to toggle the popover
-        statusItem.button?.action = #selector(togglePopover(_:))
-        statusItem.button?.target = self
-    }
-
+    // ← togglePopover stays exactly as you wrote it
     @objc func togglePopover(_ sender: AnyObject?) {
         if let button = statusItem.button {
             appPopover.toggle(relativeTo: button, sender: sender)
